abacc6740e6856d110d3c2dd2434f833a12bf38d
optydeviocourses <optydev.io.courses@gmail.com>
2022-01-22 17:59:03 -0300

    first commit

---
+++ src/root.component.tsx
@@ -0,0 +1,5 @@
+export default function Root(props) {
+  //console.log(props);
+  //return <section>{props.name} {JSON.stringify(props)} is mounted! TARGETS !!!</section>;
+  return <section>{props.name} is mounted! <b>TARGETS !!!</b></section>;
+}
